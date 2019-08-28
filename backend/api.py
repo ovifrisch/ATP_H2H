301e40231b8192e231dd54273e8fb86964a8daef
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-27 18:54:40 -0700

    context filter

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from query_atp import QueryATP
+import math
 
 atp = QueryATP()
 main = Blueprint('main', __name__)
@@ -24,7 +25,6 @@ def get_ranking_history():
 	starting_age = int(request.args.get('starting_age'))
 	ending_age = int(request.args.get('ending_age'))
 	p_info = atp.player_info(player_id)
-	print(p_info[0][3])
 	dob = parse_dt(p_info[0][3])
 	qres = atp.get_ranking_history(player_id, starting_age, ending_age)
 
@@ -55,6 +55,61 @@ def get_ranking_history():
 			filtered.append(history[-1])
 		return filtered
 
+	"""
+	motivation: if the player has been ranked at No. 1 for a long time,
+	and then moves to No.2, then we certainly want to record this
+	change in the UI. But if a player is moving around in the rankings
+	very frequently, we don't need to know about every single tiny change.
+
+	so this function takes a complete ranking history and reduces
+	it to a more compact version by collapsing consecutive rankings
+	that are the same or that are within some threshold, where this
+	threshold is low if the player's rank is numerically low, and high
+	if numerically high. The idea is that if a player has a good ranking,
+	like top 10 for example, a change of 1 ranking spot is more significant
+	than a change of 1 ranking spot for someone ranked in the top 100.
+
+	so the threshold is a function of a player's current ranking. i think a good
+	function is this:
+	lambda x => Math.floor(x/10)
+	rank 1-10 => 0
+	rank 11-20 => 1
+	rank 20-30 => 2
+	rank 30-40 => 
+
+	200-300 => 20
+	"""
+	def context_filter(history):
+
+		if (len(history) <= 2):
+			return history
+		
+
+		def threshold(rank):
+			return math.floor(rank / 10)
+
+		collapsed = [history[0]] 
+		# always add the first element
+		curr_threshold = threshold(history[0]['rank'])
+		curr_rank = history[0]['rank']
+		for i in range(1, len(history) - 1):
+			if (abs(history[i]['rank'] - curr_rank) <= curr_threshold and abs(history[i+1]['rank'] - curr_rank) <= curr_threshold):
+				continue
+			else:
+				if (collapsed[-1] != history[i]):
+					collapsed.append(history[i])
+				if (abs(history[i]['rank'] - curr_rank) > curr_threshold):
+					curr_threshold = threshold(history[i]['rank'])
+					curr_rank = history[i]['rank']
+				else:
+					curr_threshold = threshold(history[i+1]['rank'])
+					curr_rank = history[i+1]['rank']
+					collapsed.append(history[i+1])
+		if (collapsed[-1] != history[-1]):
+			collapsed.append(history[-1])
+		return collapsed
+
+
 
 	def ranking_date_to_age(rank_date, dob):
 
@@ -79,7 +134,7 @@ def get_ranking_history():
 			age = get_age(rank_date, dob)
 			return age[0] + (age[1] / 12) + ((1/12) * (age[2] / 30))
 
-	date_ranking = filter(parse(qres))
+	date_ranking = context_filter(parse(qres))
 	ranks = [x['rank'] for x in date_ranking]
 	dates = [x['date'] for x in date_ranking]
 	ages = list(map(lambda x: ranking_date_to_age(x, dob), dates))
